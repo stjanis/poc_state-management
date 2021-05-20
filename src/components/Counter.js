e1d3f829f2c458c83115120d2b14e8212f4211ed
Janis Stipnieks <janisstipnieks@Janiss-MBP-2.localdomain>
2021-05-20 10:41:13 +0200

    init commit

---
+++ src/components/Counter.js
@@ -0,0 +1,14 @@
+const Counter = () => {
+  return (
+    <div
+      style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
+    >
+      <h2>Counter</h2>
+      <p>Current count is ...</p>
+      <button>Add</button>
+      <button style={{ margin: "0 20px" }}>Remove</button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/components/Overview.js
@@ -0,0 +1,12 @@
+const Overview = () => {
+  return (
+    <div
+      style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
+    >
+      <h2>Overview</h2>
+      <p>Current Counter Count value is ...</p>
+    </div>
+  );
+};
+
+export default Overview;
