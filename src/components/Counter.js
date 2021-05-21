bb43c59a8294fb6cf4c29df91d3d025e58774d1c
Janis Stipnieks <janisstipnieks@Janiss-MacBook-Pro-2.local>
2021-05-21 14:48:50 +0200

    redux toolkit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../redux/counterSlice";
+
 const Counter = () => {
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
+  const add = () => dispatch(increment());
+  const subtract = () => dispatch(decrement());
   return (
     <div
       style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
     >
       <h2>Counter</h2>
-      <p>Current count is ...</p>
-      <button>Add</button>
-      <button style={{ margin: "0 20px" }}>Subtrack</button>
+      <p>Current count is {count}</p>
+      <button onClick={add}>Add</button>
+      <button onClick={subtract} style={{ margin: "0 20px" }}>
+        Subtrack
+      </button>
     </div>
   );
 };
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
+
 const Overview = () => {
+  const count = useSelector((state) => state.counter.count);
   return (
     <div
       style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
     >
       <h2>Overview</h2>
-      <p>Current Counter Count value is ...</p>
+      <p>Current Counter Count value is {count}</p>
     </div>
   );
 };
