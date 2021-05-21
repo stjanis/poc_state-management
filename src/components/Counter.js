2878a230f06e9319c7d780252c63b0e06c2884b2
Janis Stipnieks <janisstipnieks@Janiss-MBP-2.localdomain>
2021-05-21 14:24:17 +0200

    recoil

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,24 @@
+import { atom, useRecoilState } from "recoil";
+
+export const counterState = atom({
+  key: "counterState",
+  default: 0,
+});
+
 const Counter = () => {
+  const [count, setCount] = useRecoilState(counterState);
+  const add = () => setCount(count + 1);
+  const subtract = () => setCount(count - 1);
   return (
     <div
       style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
     >
       <h2>Counter</h2>
-      <p>Current count is ...</p>
-      <button>Add</button>
-      <button style={{ margin: "0 20px" }}>Subtrack</button>
+      <p>Current count is {count}</p>
+      <button onClick={add}>Add</button>
+      <button onClick={subtract} style={{ margin: "0 20px" }}>
+        Subtrack
+      </button>
     </div>
   );
 };
--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,10 +1,25 @@
+import { useRecoilValue, selector } from "recoil";
+import { counterState } from "./Counter";
+
+const taxesSummary = selector({
+  key: "taxesSummary",
+  get: ({ get }) => {
+    const count = get(counterState);
+    return count * 1.5;
+  },
+});
+
 const Overview = () => {
+  const currentCount = useRecoilValue(counterState);
+  const taxValue = useRecoilValue(taxesSummary);
+
   return (
     <div
       style={{ background: "#f1f1f1", padding: "20px", marginBottom: "20px" }}
     >
       <h2>Overview</h2>
-      <p>Current Counter Count value is ...</p>
+      <p>Current Counter Count value is {currentCount}</p>
+      <p>Current Counter Count value is {taxValue}</p>
     </div>
   );
 };
